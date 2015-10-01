7404145e17401fb115c1af5f28888773b194737a
Ehsan Rafeeque <ehsan5505@gmail.com>
2015-10-01 21:29:00 +0500

    adding the basic template model for testing yield fields

---
+++ resources/views/ehsan.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE>
+<html>
+	<head>
+		<title>Main Template design preferences</title>
+	</head>
+	<body>
+		<header>
+			<hgroup>
+			<h1>ESS SYSTEM Test </h1>
+			</hgroup>
+		</header>
+
+		<footer>
+			<p>&copy; All Rights Reserved By System</p>
+		</footer>
+
+	</body>
+</html>
