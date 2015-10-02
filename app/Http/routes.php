1d4e84386c91d40adb2aa4361bbe7fbe4079dd69
Ehsan Rafeeque <ehsan5505@gmail.com>
2015-10-02 06:21:25 +0500

    Just the app changes where route changes perform the GET of /main , /pagination , /product to cater the sample pages of the https://www.topresellerstore.it

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,3 +18,19 @@ Route::get('/', function () {
 Route::get('/ehsan',function(){
 	return view('ehsan');
 });
+
+Route::get('/test',function(){
+	return view('test');
+});
+
+Route::get('/main',function(){
+	return view('main');
+});
+
+Route::get('/pagination',function(){
+	return view('pagination');
+});
+
+Route::get('product',function(){
+	return view('product');
+});
