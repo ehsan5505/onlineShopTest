a14f7b28fcee485b271b58155823aa5f4edd2dda
Ehsan Rafeeque <ehsan5505@gmail.com>
2015-10-02 06:19:24 +0500

    adding the css and test where test.php main purpose is to check the component of the frontEndTool

---
+++ resources/views/test.php
@@ -0,0 +1,92 @@
+<!DOCTYPE>
+<html>
+	<head>
+		<title>System is working fine</title>
+		<link rel="stylesheet" href="style.css">
+		<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
+		<link rel="stylesheet" href="http://yui.yahooapis.com/combo?pure/0.6.0/base-min.css&pure/0.6.0/grids-min.css&pure/0.6.0/grids-responsive-min.css">
+	</head>
+	<body>
+		<!-- <div class="pure-g" style="background:pink">
+			<div class="pure-u-1-2" style="background:red">This is 1</div>
+			<div class="pure-u-1-3" style="background:green">This is 2</div>
+			<div class="pure-u-1-3" style="background:blue">This is 3</div>
+		</div>
+
+		<div class="pure-g">
+	    	<div class="pure-u-1-3"><p>Thirds</p></div>
+	    	<div class="pure-u-2-3"><p>Thirds</p></div>   
+		</div> -->
+		<!-- <div class="pure-g">
+	    <div class="pure-u-sm-1-3"><p>Thirds</p></div>
+	    <div class="pure-u-sm-1-3"><p>Thirds</p></div>
+	    <div class="pure-u-sm-1-3"><p>Thirds</p></div>
+	</div>
+		<div class="pure-g">
+			<div class="pure-u-lg-1-1">This is 1</div>
+			<div class="pure-u-lg-1-1">This is 2</div>
+			<div class="pure-u-lg-1-1">This is 3</div>
+			<div class="pure-u-lg-1-1">This is 4</div>
+			<div class="pure-u-lg-1-1">This is 5</div>
+			<div class="pure-u-lg-1-1">This is 6</div>
+			<div class="pure-u-lg-1-1">This is 7</div>
+			<div class="pure-u-lg-1-1">This is 8</div>
+		</div> -->
+
+	<div class="pure-g">
+	 		<div class="pure-u-lg-1-8 pure-u-md-1-3 pure-u-sm-1-2 pure-u-1">1 Ehsan Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus sunt labore delectus placeat, deserunt perspiciatis laborum assumenda blanditiis, ipsum numquam. Omnis esse, aut delectus aspernatur nesciunt, expedita reprehenderit rem fugit.</div>
+	 		<div class="pure-u-lg-1-8 pure-u-md-1-3 pure-u-sm-1-2 pure-u-1">2 Ehsan Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus sunt labore delectus placeat, deserunt perspiciatis laborum assumenda blanditiis, ipsum numquam. Omnis esse, aut delectus aspernatur nesciunt, expedita reprehenderit rem fugit.</div>
+	 		<div class="pure-u-lg-1-8 pure-u-md-1-3 pure-u-sm-1-2 pure-u-1">3 Ehsan Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus sunt labore delectus placeat, deserunt perspiciatis laborum assumenda blanditiis, ipsum numquam. Omnis esse, aut delectus aspernatur nesciunt, expedita reprehenderit rem fugit.</div>
+	 		<div class="pure-u-lg-1-8 pure-u-md-1-3 pure-u-sm-1-2 pure-u-1">4 Ehsan Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus sunt labore delectus placeat, deserunt perspiciatis laborum assumenda blanditiis, ipsum numquam. Omnis esse, aut delectus aspernatur nesciunt, expedita reprehenderit rem fugit.</div>
+	 		<div class="pure-u-lg-1-8 pure-u-md-1-3 pure-u-sm-1-2 pure-u-1">5 Ehsan Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus sunt labore delectus placeat, deserunt perspiciatis laborum assumenda blanditiis, ipsum numquam. Omnis esse, aut delectus aspernatur nesciunt, expedita reprehenderit rem fugit.</div>
+	 		<div class="pure-u-lg-1-8 pure-u-md-1-3 pure-u-sm-1-2 pure-u-1">6 Ehsan Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus sunt labore delectus placeat, deserunt perspiciatis laborum assumenda blanditiis, ipsum numquam. Omnis esse, aut delectus aspernatur nesciunt, expedita reprehenderit rem fugit.</div>
+	 		<div class="pure-u-lg-1-8 pure-u-md-1-3 pure-u-sm-1-2 pure-u-1">7 Ehsan Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus sunt labore delectus placeat, deserunt perspiciatis laborum assumenda blanditiis, ipsum numquam. Omnis esse, aut delectus aspernatur nesciunt, expedita reprehenderit rem fugit.</div>
+			<div class="pure-u-lg-1-8 pure-u-md-1-3 pure-u-sm-1-2 pure-u-1">8 Ehsan Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus sunt labore delectus placeat, deserunt perspiciatis laborum assumenda blanditiis, ipsum numquam. Omnis esse, aut delectus aspernatur nesciunt, expedita reprehenderit rem fugit.</div>		
+	</div>
+	<div class="pure-u-1">
+		<form class="pure-form pure-form-aligned">
+    <fieldset>
+        <div class="pure-control-group">
+            <label for="name">Username</label>
+            <input id="name" type="text" placeholder="Username">
+        </div>
+
+        <div class="pure-control-group">
+            <label for="password">Password</label>
+            <input id="password" type="password" placeholder="Password">
+        </div>
+
+        <div class="pure-control-group">
+            <label for="email">Email Address</label>
+            <input id="email" type="email" placeholder="Email Address">
+        </div>
+
+        <div class="pure-control-group">
+            <label for="foo">Supercalifragilistic Label</label>
+            <input id="foo" type="text" placeholder="Enter something here...">
+        </div>
+
+        <div class="pure-controls">
+            <label for="cb" class="pure-checkbox">
+                <input id="cb" type="checkbox"> I've read the terms and conditions
+            </label>
+
+            <button type="submit" class="pure-button pure-button-primary">Submit</button>
+        </div>
+    </fieldset>
+</form>
+	</div>
+	<!-- <div class="pure-g">		 -->
+			<!-- <div class="pure-u-md-1-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae vero aliquid perspiciatis corporis ea quod quas, nihil id rem nam aut consectetur sunt ipsa omnis aspernatur repellat laboriosam quam veniam!</div>
+			<div class="pure-u-md-1-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae vero aliquid perspiciatis corporis ea quod quas, nihil id rem nam aut consectetur sunt ipsa omnis aspernatur repellat laboriosam quam veniam!</div>
+			<div class="pure-u-md-1-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae vero aliquid perspiciatis corporis ea quod quas, nihil id rem nam aut consectetur sunt ipsa omnis aspernatur repellat laboriosam quam veniam!</div>
+	
+			<div class="pure-u-sm-1-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati sint id consequuntur, facere omnis. Quibusdam dignissimos unde, cupiditate cumque placeat, aperiam necessitatibus iusto sint nobis doloribus dolorum nulla, ad molestias!</div>
+			<div class="pure-u-sm-1-1">Quam blanditiis voluptatibus sapiente eos, voluptates culpa quo ipsam molestias alias recusandae architecto quia exercitationem corporis earum, maiores nam excepturi provident sint eum veniam. Saepe modi vero quam nihil voluptatem.</div>
+			<div class="pure-u-sm-1-1">Rem magni aliquam architecto minima, nobis tempora voluptatibus autem suscipit eveniet veniam sunt quas, commodi impedit voluptatem maiores illum ex porro enim, reprehenderit quasi! Repellat fugiat non accusantium saepe doloremque.</div> -->
+	<!-- </div> -->
+	<!-- <div class="pure-g"> -->
+	<!-- </div> -->
+ 	</body>
+
+</html>
